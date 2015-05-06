93fa8de73ffd9523b126ad20beea3ece457d6e81
Daniel Eden <dan.eden@me.com>
2015-05-06 16:31:00 -0700

    Automated commit at 2015-05-06 23:31:21 UTC by middleman-deploy 1.0.0

--- assets/js/all.js
+++ assets/js/all.js
@@ -11,3 +11,8 @@
   }
 
 }).call(this);
+var navBar = document.querySelector('.sc-site__nav');
+var activeLink = document.querySelector('.is-active');
+
+
+navBar.scrollLeft = (activeLink.parentNode.offsetLeft - 30);
